451af240260571d54b60ca1f52e74e5504a27a14
sivaram <sivaram.p842@gmail.com>
2025-03-07 14:57:43 +0530

    registerUser api

--- backend/controlers/userController.js
+++ backend/controlers/userController.js
@@ -6,17 +6,50 @@ import validator from "validator";
 
 //login User 
 
-const loginUser = async (res, req) => {
+const loginUser = async (req, res) => {
 
+}
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
     try {
+        //checking if user already exists
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, message: "User already exists" })
+        }
 
-    } catch (error) {
+        //validating email and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        }
 
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Password is short" })
+        }
+
+        //hashing user password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashPassword
+        })
+        const user = await newUser.save();
+        const token = createToken(user._id)
+        res.json({ success: true, token })
+    }
+    catch (error) {
+        console.log(error);
+        res.json({ success: true, message: "Error" })
     }
 }
+
 
 export { loginUser, registerUser };
